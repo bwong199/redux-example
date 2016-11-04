109b67b42c7fcca2a125933e61b5884a8a53b03a
bwong199 <bwong199@my.bcit.ca>
2016-11-03 23:01:34 -0600

    Added countdownform test

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,12 @@ var Main = (props) => {
 		<div>
 			<div>
 				<Navigation />
-				{props.children}
+				<div className="row">
+					<div className="column small-centered medium-6 large-4">
+					{props.children}
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
