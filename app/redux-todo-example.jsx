0fc7bf479909e7d841f5ff989d79a1aaf917e563
bwong199 <bwong199@my.bcit.ca>
2016-11-05 11:31:33 -0600

    Add support for CHANGE_NAME action

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,10 +9,29 @@ var stateDefault = {
 }
 
 var reducer = (state = stateDefault, action) => {
-	return state
+
+	console.log('New action,', action);
+	switch (action.type){
+		case 'CHANGE_SEARCH_TEXT': 
+			return  {
+				...state,
+				searchText: action.searchText
+			};
+		default:
+			return state;
+	}	
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+store.dispatch({
+	type: 'CHANGE_SEARCH_TEXT', 
+	searchText: 'work'
+});
+
+console.log('serachText should be "work"', store.getState());
+
